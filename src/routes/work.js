201d5f8f4f9307276084d55cf0235b54805480ff
Gourav Kiran Kolhatkar <gstar1525@gmail.com>
2021-03-29 11:22:35 +0530

    Added Wrok Section Layout

--- src/routes/work.js
+++ src/routes/work.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "./card"
 
-function Work(){
+const useStyles = makeStyles({
+    gridContainer: {
+        paddingLeft: "40px",
+        paddingRight: "40px"
+    }
+});
+
+
+function Work() {
+    const classes = useStyles();
     return (
+        <>
         <h1>
             Work
         </h1>
+        <Grid
+            container
+            spacing={4}
+            className={classes.gridContainer}
+            justify="center"
+        >
+            <Grid item xs={12} sm={6} md={4}>
+                <Card />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card />
+            </Grid>
+        </Grid
+        ></>
     );
 }
 
